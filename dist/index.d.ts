89f1cf48eca2b9e6343d31ef5f685c3a7d10bb12
rupansh <rupansh@users.noreply.github.com>
2022-06-20 17:05:27 +0000

    Build: (88f934b) workflows: gen-typechain: fix repo url

---
+++ dist/index.d.ts
@@ -0,0 +1,8 @@
+export type { Bridge } from "./Bridge";
+export type { DummyErc20 } from "./DummyErc20";
+export type { SchnorrSECP256K1 } from "./SchnorrSECP256K1";
+export type { WXPNET } from "./WXPNET";
+export { Bridge__factory } from "./factories/Bridge__factory";
+export { DummyErc20__factory } from "./factories/DummyErc20__factory";
+export { SchnorrSECP256K1__factory } from "./factories/SchnorrSECP256K1__factory";
+export { WXPNET__factory } from "./factories/WXPNET__factory";
---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.WXPNET__factory = exports.SchnorrSECP256K1__factory = exports.DummyErc20__factory = exports.Bridge__factory = void 0;
+var Bridge__factory_1 = require("./factories/Bridge__factory");
+Object.defineProperty(exports, "Bridge__factory", { enumerable: true, get: function () { return Bridge__factory_1.Bridge__factory; } });
+var DummyErc20__factory_1 = require("./factories/DummyErc20__factory");
+Object.defineProperty(exports, "DummyErc20__factory", { enumerable: true, get: function () { return DummyErc20__factory_1.DummyErc20__factory; } });
+var SchnorrSECP256K1__factory_1 = require("./factories/SchnorrSECP256K1__factory");
+Object.defineProperty(exports, "SchnorrSECP256K1__factory", { enumerable: true, get: function () { return SchnorrSECP256K1__factory_1.SchnorrSECP256K1__factory; } });
+var WXPNET__factory_1 = require("./factories/WXPNET__factory");
+Object.defineProperty(exports, "WXPNET__factory", { enumerable: true, get: function () { return WXPNET__factory_1.WXPNET__factory; } });
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBUUEsK0RBQThEO0FBQXJELGtIQUFBLGVBQWUsT0FBQTtBQUN4Qix1RUFBc0U7QUFBN0QsMEhBQUEsbUJBQW1CLE9BQUE7QUFDNUIsbUZBQWtGO0FBQXpFLHNJQUFBLHlCQUF5QixPQUFBO0FBQ2xDLCtEQUE4RDtBQUFyRCxrSEFBQSxlQUFlLE9BQUEifQ==
